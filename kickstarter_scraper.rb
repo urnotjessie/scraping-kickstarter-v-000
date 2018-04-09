b6241430f9c8651ee06dd2789e5595f4f129fa41
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-09 17:18:31 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -17,5 +17,6 @@ def create_project_hash
       percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
   end
-  puts projects
+  projects
+  binding.pry
 end
