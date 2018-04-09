9a26af4ea83eeddb0aabc975afc6bd51d05df941
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-09 17:09:02 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,3 +14,4 @@ create_project_hash
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 # location: project.css("ul.project-meta li a span.location-name").text
+project.css("ul.project-status li strong").text
